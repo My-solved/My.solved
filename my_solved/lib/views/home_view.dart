72b98e421889214f758aefe4324ff2e034f4d8c2
jayden000106 <cars0106@icloud.com>
2022-11-15 03:55:46 +0900

    feat: 상단 이미지, 버튼 배치

--- my_solved/lib/views/home_view.dart
+++ my_solved/lib/views/home_view.dart
@@ -6,7 +6,61 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: SafeArea(child: Text("Home")),
+      child: SafeArea(
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                profileHeader(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+extension HomeViewExtension on HomeView {
+  Widget profileHeader(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Container(
+        margin: EdgeInsets.only(bottom: 50),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              color: CupertinoColors.systemGrey,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: CupertinoColors.activeGreen,
+                    shape: BoxShape.circle,
+                  ),
+                  margin: EdgeInsets.only(top: 100, left: 20),
+                ),
+                Spacer(),
+                CupertinoButton(
+                  onPressed: () {},
+                  child: Icon(
+                    CupertinoIcons.gear_alt_fill,
+                    color: CupertinoColors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
