bbe0bac2d89dae1710cd2614cd8afb3af3635b22
jayden000106 <cars0106@icloud.com>
2022-11-15 04:09:05 +0900

    feat: 설정 뷰 네비게이션 바 구현

--- my_solved/lib/views/setting_view.dart
+++ my_solved/lib/views/setting_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SettingView extends StatelessWidget {
   const SettingView({Key? key}) : super(key: key);
@@ -6,7 +7,39 @@ class SettingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: SafeArea(child: Text("Setting")),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('설정'),
+      ),
+      child: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(top: 10, right: 20, left: 20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Text('백준 ID'),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('백준 ID'),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('백준 ID'),
+                ],
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Text('백준 ID'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
