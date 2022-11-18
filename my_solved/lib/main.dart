1e111eeb0b56053ee6996ce3741cd5052ba6e09c
w8385 <w8385@naver.com>
2022-11-19 03:23:58 +0900

    feat: 세로모드 고정

--- my_solved/lib/main.dart
+++ my_solved/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:my_solved/pages/main_tab_page.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return CupertinoApp(
       home: MainTabPage(),
     );
