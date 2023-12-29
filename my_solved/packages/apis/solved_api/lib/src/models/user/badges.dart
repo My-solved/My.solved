bcd8b9ae798f0ccb64c09cecd88bdd624bd4fae1
w8385 <w8385@naver.com>
2023-12-30 02:17:43 +0900

    refactor: Badges 모델 수정

--- my_solved/packages/apis/solved_api/lib/src/models/user/badges.dart
+++ my_solved/packages/apis/solved_api/lib/src/models/user/badges.dart
@@ -1,3 +1,5 @@
+import 'package:solved_api/src/models/badge.dart';
+
 class Badges {
   final int count;
   final List<dynamic> items;
@@ -10,7 +12,7 @@ class Badges {
   factory Badges.fromJson(Map<String, dynamic> json) {
     return Badges(
       count: json['count'],
-      items: json['items'],
+      items: json['items'].map((i) => Badge.fromJson(i)).toList().cast<Badge>(),
     );
   }
 }
