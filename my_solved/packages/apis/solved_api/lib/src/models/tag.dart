691841121da5981723eb3943bd2677565c19f3e8
w8385 <w8385@naver.com>
2024-01-01 02:49:21 +0900

    refactor: ProblemTag => Tag

--- my_solved/packages/apis/solved_api/lib/src/models/tag.dart
+++ my_solved/packages/apis/solved_api/lib/src/models/tag.dart
@@ -1,22 +1,28 @@
-class ProblemTag {
+class Tag {
   final String key;
   final bool isMeta;
   final int bojTagId;
   final int problemCount;
+  final List<dynamic> displayNames;
+  final List<dynamic> aliases;
 
-  const ProblemTag({
+  const Tag({
     required this.key,
     required this.isMeta,
     required this.bojTagId,
     required this.problemCount,
+    required this.displayNames,
+    required this.aliases,
   });
 
-  factory ProblemTag.fromJson(Map<String, dynamic> json) {
-    return ProblemTag(
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
       key: json['key'],
       isMeta: json['isMeta'],
       bojTagId: json['bojTagId'],
       problemCount: json['problemCount'],
+      displayNames: json['displayNames'],
+      aliases: json['aliases'],
     );
   }
 }
