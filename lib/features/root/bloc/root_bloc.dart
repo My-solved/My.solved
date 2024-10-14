e1c4a4f6b9127f7b88070a221baabc51a5d66f7f
정지혁 <cars0106@icloud.com>
2024-10-14 21:41:46 +0900

    feat: 버전 정보 가져오기 구현

--- lib/features/root/bloc/root_bloc.dart
+++ lib/features/root/bloc/root_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 part 'root_event.dart';
 part 'root_state.dart';
@@ -9,6 +10,12 @@ class RootBloc extends Bloc<RootEvent, RootState> {
   final String handle;
 
   RootBloc({required this.handle}) : super(RootState(handle: handle)) {
+    on<VersionInfoInit>(
+      (event, emit) async {
+        final packageInfo = await PackageInfo.fromPlatform();
+        emit(state.copyWith(version: packageInfo.version));
+      },
+    );
     on<NavigationBarItemTapped>(
       (event, emit) => emit(state.copyWith(tabIndex: event.tabIndex)),
     );
--- lib/features/root/bloc/root_state.dart
+++ lib/features/root/bloc/root_state.dart
@@ -3,19 +3,26 @@ part of 'root_bloc.dart';
 class RootState extends Equatable {
   final String handle;
   final int tabIndex;
+  final String version;
 
-  const RootState({required this.handle, this.tabIndex = 0});
+  const RootState({
+    required this.handle,
+    this.tabIndex = 0,
+    this.version = "",
+  });
 
   RootState copyWith({
     String? handle,
     int? tabIndex,
+    String? version,
   }) {
     return RootState(
       handle: handle ?? this.handle,
       tabIndex: tabIndex ?? this.tabIndex,
+      version: version ?? this.version,
     );
   }
 
   @override
-  List<Object?> get props => [handle, tabIndex];
+  List<Object?> get props => [handle, tabIndex, version];
 }
