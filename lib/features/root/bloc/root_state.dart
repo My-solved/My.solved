ed8dadf3a34cc9543e30481430e9bdc33e9d4438
정지혁 <cars0106@icloud.com>
2024-10-14 22:15:01 +0900

    feat: 버전 정보 반영

--- lib/features/root/bloc/root_state.dart
+++ lib/features/root/bloc/root_state.dart
@@ -1,28 +1,43 @@
 part of 'root_bloc.dart';
 
-class RootState extends Equatable {
-  final String handle;
+abstract class RootState extends Equatable {
   final int tabIndex;
-  final String version;
 
-  const RootState({
-    required this.handle,
-    this.tabIndex = 0,
-    this.version = "",
+  const RootState({required this.tabIndex});
+
+  RootState copyWith({int? tabIndex});
+
+  @override
+  List<Object?> get props => [tabIndex];
+}
+
+class RootInitial extends RootState {
+  const RootInitial({super.tabIndex = 0});
+
+  @override
+  RootInitial copyWith({int? tabIndex}) {
+    return RootInitial(
+      tabIndex: tabIndex ?? this.tabIndex,
+    );
+  }
+}
+
+class RootSuccess extends RootState {
+  const RootSuccess({
+    required super.tabIndex,
+    required this.version,
   });
 
-  RootState copyWith({
-    String? handle,
-    int? tabIndex,
-    String? version,
-  }) {
-    return RootState(
-      handle: handle ?? this.handle,
+  final String version;
+
+  @override
+  RootSuccess copyWith({int? tabIndex, String? version}) {
+    return RootSuccess(
       tabIndex: tabIndex ?? this.tabIndex,
       version: version ?? this.version,
     );
   }
 
   @override
-  List<Object?> get props => [handle, tabIndex, version];
+  List<Object?> get props => [...super.props, version];
 }
